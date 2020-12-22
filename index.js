f4895873c78d050805ee49bb7e23bd632d5cf17e
cameron-poehner <cameronpoehner@gmail.com>
2020-12-22 12:10:11 -0800

    added queryselectors, callback funtion, and eventlisteners

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('connected!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = document.querySelectorAll('[role="tab"]');
+const tabPanels = document.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+    // Hide all tab panels
+    tabPanels.forEach(panel => panel.hidden = true);
+    // Mark all tab panels as selected
+    tabButtons.forEach(tab => 
+        tab.setAttribute('aria-selected', false));
+    // mark the clicked Tab as selected
+    event.currentTarget.setAttribute('aria-selected', true);    
+    // Find the associated tab panel and show it
+    const { id } = event.currentTarget;
+    const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+    console.log(tabPanel);
+    tabPanel.hidden = false;
+
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
